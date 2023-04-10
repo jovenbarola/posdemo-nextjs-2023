d19bc1d04a0873734c99325f2b8f8e64fc3d8d98
jovenbarola <jovenbarola@gmail.com>
2023-04-10 22:00:37 +0800

    3 files modified

--- pages/api/tables.js
+++ pages/api/tables.js
@@ -1,10 +1,25 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import mongoose from 'mongoose';
 
-let sconnectionstring =
-  'mongodb+srv://barolajoven:<password>@cluster0.mboz1bc.mongodb.net/?retryWrites=true&w=majority';
+const { Schema } = mongoose;
+
+const tables = new Schema({
+  title: String, // String is shorthand for {type: String}
+  author: String,
+  body: String,
+  comments: [{ body: String, date: Date }],
+  date: { type: Date, default: Date.now },
+  hidden: Boolean,
+  meta: {
+    votes: Number,
+    favs: Number,
+  },
+});
 
 export default (req, res) => {
-  // Open Chrome DevTools to step through the debugger!
+  const dbconstring =
+    'mongodb+srv://barolajoven:<password>@cluster0.mboz1bc.mongodb.net/posdemo?retryWrites=true&w=majority';
+  const result = mongoose.connect(dbconstring);
 
   res.status(200).json({ name: 'Hello, world!' });
 };
